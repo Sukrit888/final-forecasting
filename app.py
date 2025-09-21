59cb4c8205a462cf444804aead1fed107706c1b4
Sukrit Kashyap Goswami <6sukritgoswami@gmail.com>
2025-09-22 03:16:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -107,7 +107,7 @@ IMPACT_FACTORS = {
     }
 }
 
-API_KEY = ""
+API_KEY = "AIzaSyAxwsGmdKXduSjtKvZKYhjnlFfwrpcxkKU"
 API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-preview-05-20:generateContent"
 
 def generate_note_with_api(input_reasoning):
